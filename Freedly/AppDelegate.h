fa51a8acbe8b319d7e1129baf3a48bf75edaa5f1
Alejandro Silva Fernández <h4ll0ck@gmail.com>
2014-02-26 23:35:12 +0100

    Code upload

---
+++ Freedly/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Feedly
+//
+//  Created by Alejandro Silva on 26/02/14.
+//  Copyright (c) 2014 Alejandro Silva. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Freedly/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  Feedly
+//
+//  Created by Alejandro Silva on 26/02/14.
+//  Copyright (c) 2014 Alejandro Silva. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+
+}
+
+@end
